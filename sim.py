2dce462ad7057345ce66e03a508d5dc67b38d61e
ad1v7 <marcin.kirsz@gmail.com>
2017-10-16 09:15:47 +0100

    Example class layout with basic functions

---
+++ sim.py
@@ -0,0 +1,89 @@
+class Landscape(object):
+    def __init__(self, filename):
+        self.landscape = self.load_landscape(filename)
+        self.dry_squares = self.find_dry_squares()
+
+    def load_landscape(self, filename):
+        print('Loading landscape')
+        return []
+
+    def find_dry_squares(self):
+        print('calculating number of dry squares')
+        return []
+
+class Population(object):
+    def __init__(self, Landscape, birth, death, diffusion, min_ro, max_ro):
+        self.min_ro = min_ro
+        self.max_ro = max_ro
+        self.birth = birth
+        self.death = death
+        self.diffusion = diffusion
+        self.density = self.random_density(Landscape)
+
+    def random_density(self, Landscape):
+        min_ro = self.min_ro
+        max_ro = self.max_ro
+        grid = Landscape.landscape
+        print('Random distribution')
+
+class PumaPopulation(Population):
+    def __init__(self, Landscape, birth=1.01, death=2.02, diffusion=3.03,
+                 min_ro=0, max_ro=5):
+        self.kind = 'PumaPopulation'
+        super(PumaPopulation, self).__init__(Landscape, birth, death,
+                                             diffusion, min_ro, max_ro)
+        print('Puma population created')
+
+    # alternatively
+    # def update_density(self, puma_pop, hare_pop):
+    # which is slightly more messy to implement as you need to keep track of
+    # arguments and their order but is arguably more efficient (how much?)
+    def update_density(self, populations):
+        puma_pop = next((pop for pop in populations if
+                    pop.kind=='PumaPopulation'), None)
+        print(puma_pop.kind)
+        hare_pop = next((pop for pop in populations if
+                    pop.kind=='HarePopulation'), None) # None is default value
+        # set X_pop.density to zeroes if no pop in populations
+        print(self.kind +' density updated')
+        # set ro=0 if ro<0
+
+# class Environment() ?
+class Simulation():
+    def __init__(self, Landscape, *args):
+        # create populations list but ignore args which are not populations
+        self.populations = [pop for pop in args if isinstance(pop, Population)]
+        print('Running simulation')
+
+    # not sure is this function required
+    def add_population(self, pop):
+        return self.populations.append(pop)
+
+    # not sure is this function required
+    def remove_population(self, pop):
+        print(pop.kind + ' removed')
+
+    def update(self):
+        for pop in self.populations:
+            pop.update_density(self.populations)
+
+# create new landscape from the file 'my_land'
+env = Landscape('my_land')
+
+# create new population using default values
+puma_pop = PumaPopulation(env)
+print(puma_pop.birth)
+print(puma_pop.death)
+print(puma_pop.max_ro)
+print('#######')
+
+# create new population using specific values (config file...)
+puma_pop = PumaPopulation(env, death=9.999, max_ro=55)
+print(puma_pop.birth)
+print(puma_pop.death)
+print(puma_pop.max_ro)
+
+# create new simulation with the landscape env and puma population
+sim = Simulation(env, puma_pop)
+# update one step
+sim.update()
