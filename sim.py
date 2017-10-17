8b04b610d120fc37929519d533aeb680c46f773a
ad1v7 <marcin.kirsz@gmail.com>
2017-10-17 22:02:48 +0100

    Initial implementation of Hare/Puma and Simulation Classes, including respective update functions

--- sim.py
+++ sim.py
@@ -4,12 +4,12 @@ import numpy as np
     # Chloe
 class Config():
     def __init__(self, config_file):
-        self.r = 0.0 
-        self.d = 0.0 
+        self.r = 0.
+        self.d = 0.
 
     def load_from_file(self):
-        parse config file and assign self.r etc
-        
+        pass
+
 
 class Landscape(object):
     def __init__(self, filename):
@@ -18,7 +18,6 @@ class Landscape(object):
 
     def load_landscape(self, filename):
         print('Loading landscape')
-        return np.array(M x N)
 
     def find_dry_squares(self):
         print('calculating number of dry squares')
@@ -34,66 +33,120 @@ class Population(object):
         self.diffusion = diffusion
         self.density = self.random_density(Landscape)
         
-    Elen
+    #Elen
     def random_density(self, Landscape):
         min_ro = self.min_ro
         max_ro = self.max_ro
         grid = Landscape.landscape
         print('Random distribution')
-        return np.array(of size landscape)
 
-Marcin
+# ToDo
+# where N array is comming from?
+# where dT is stored?
+# apart from that it is almost there...
+
 class PumaPopulation(Population):
-    def __init__(self, Landscape, birth=1.01, death=2.02, diffusion=3.03,
-                 min_ro=0, max_ro=5):
-        self.kind = 'PumaPopulation'
+    def __init__(self, Landscape, birth=.02, death=.06, diffusion=.02,
+                 min_ro=0., max_ro=5.):
         super(PumaPopulation, self).__init__(Landscape, birth, death,
                                              diffusion, min_ro, max_ro)
         print('Puma population created')
 
-    # alternatively
-    # def update_density(self, puma_pop, hare_pop):
-    # which is slightly more messy to implement as you need to keep track of
-    # arguments and their order but is arguably more efficient (how much?)
-    def update_density(self, populations):
-        # set X_pop.density to zeroes if no pop in populations
-        # this should allow computation to proceed (verify that)
-        zero_density_arr = np.zeros(self.density.shape)
-        puma_pop = next((pop.density for pop in populations if
-                         pop.kind == 'PumaPopulation'), zero_density_arr)
-        hare_pop = next((pop.density for pop in populations if
-                         pop.kind == 'HarePopulation'), zero_density_arr)
-        print(hare_pop)
-        print(puma_pop)
-        print(self.kind + ' density updated')
-        # set ro=0 if ro<0
+    def update_density(self, old_populations, populations):
+        # extract required populations density arrays for update
+        # and assign to reference arrays
+        # if no populations found assign zero density array
+        rows, cols = self.density.shape
+        zero_density_arr = np.zeros((rows, cols))
+        P_new = next((pop.density for pop in populations if
+                      isinstance(pop, PumaPopulation)), zero_density_arr)
+        P = next((pop.density for pop in old_populations if
+                  isinstance(pop, PumaPopulation)), zero_density_arr)
+        H = next((pop.density for pop in old_populations if
+                  isinstance(pop, HarePopulation)), zero_density_arr)
+
+        for i in range(rows):
+            for j in range(cols):
+                P_new[i][j] = self.update_density_ij(i, j, P, H)
+
+    def update_density_ij(self, i, j, P, H):
+        b = self.birh
+        m = self.death
+        l = self.diffusion
+        return P[i][j] + dT * (b * H[i][j] * P[i][i] - m * P[i][j]
+                               + l * ((P[i-1][j] + P[i+1][j] + P[i][j-1] + P[i][j+1])
+                                      - N[i][j] * P[i][j]))
+
+class HarePopulation(Population):
+    def __init__(self, Landscape, birth=.08, death=.04, diffusion=.02,
+                 min_ro=0., max_ro=5.):
+        super(HarePopulation, self).__init__(Landscape, birth, death,
+                                             diffusion, min_ro, max_ro)
+        print('Hare population created')
+
+    def update_density(self, old_populations, populations):
+        # extract required populations density arrays for update
+        # and assign to reference arrays
+        # if no populations assign zero density array
+        rows, cols = self.density.shape
+        zero_density_arr = np.zeros((rows, cols))
+        H_new = next((pop.density for pop in populations if
+                      isinstance(pop, HarePopulation)), zero_density_arr)
+        P = next((pop.density for pop in old_populations if
+                  isinstance(pop, PumaPopulation)), zero_density_arr)
+        H = next((pop.density for pop in old_populations if
+                  isinstance(pop, HarePopulation)), zero_density_arr)
+
+        # update array but ommit water boundary
+        # ToDo
+        # iterate i,j which are land only (skip water)
+        for i in range(1, rows-1):
+            for j in range(1, cols-1):
+                H_new[i][j] = self.update_density_ij(i, j, P, H)
+
+    def update_density_ij(self, i, j, P, H):
+        r = self.birh
+        a = self.death
+        k = self.diffusion
+        return H[i][j] + dT * (r * H[i][j] - a * H[i][j] * P[i][j]
+                               + k * ((H[i-1][j] + H[i+1][j] + H[i][j-1] + H[i][j+1])
+                                      - N[i][j] * H[i][j]))
 
 
-# class Environment() ?
+
 class Simulation():
-Marcin
     def __init__(self, Landscape, *args):
         # create populations list but ignore args which are not populations
         self.populations = [pop for pop in args if isinstance(pop, Population)]
-        print('Running simulation')
 
     # not sure is this function required
     def add_population(self, pop):
-        return self.populations.append(pop)
-Marcin
+        self.populations.append(pop)
+
     # not sure is this function required
     def remove_population(self, pop):
-        print(pop.kind + ' removed')
-    Marcin
-    def update(self):
-        for pop in self.populations:
-            pop.update_density(self.populations)
-     
-    def run(self):
-        for loop
-    Elen
+        self.populations.remove(pop)
+
+    def update(self, old_populations, populations):
+        for pop in populations:
+            pop.update_density(old_populations, populations)
+
+    def run(self, num_steps):
+        print('Running simulation')
+        old_populations = np.copy(self.populations)
+        for i in range(num_steps):
+            if i % 2 == 0:
+                self.update(old_populations, self.populations)
+            else:
+                self.update(self.populations, old_populations)
+
+        # make sure we return last updated array
+        if num_steps % 2 == 0:
+            self.populations = np.copy(old_populations)
+
+    #Elen
     def save_state(self):
-        save densities to ppm file
+        pass
 
 # create new landscape from the file 'my_land'
 env = Landscape('my_land')
@@ -106,12 +159,12 @@ print(puma_pop.max_ro)
 print('#######')
 
 # create new population using specific values (config file...)
-puma_pop = PumaPopulation(env, death=9.999, max_ro=55)
-print(puma_pop.birth)
-print(puma_pop.death)
-print(puma_pop.max_ro)
+hare_pop = HarePopulation(env)
+print(hare_pop.birth)
+print(hare_pop.death)
+print(hare_pop.max_ro)
 
 # create new simulation with the landscape env and puma population
 sim = Simulation(env, puma_pop)
 # update one step
-sim.update()
+#sim.run(20)
