3e8a17cf4f181ec4310efaaf60733da18c4546f8
ad1v7 <marcin.kirsz@gmail.com>
2017-10-24 21:57:36 +0100

    Added example unit test

--- pumha/test/test_sim.py
+++ pumha/test/test_sim.py
@@ -1,1 +1,14 @@
+from unittest import TestCase
+from pumha import sim
+import numpy
+
+class TestPopulation(TestCase):
+    def test_find_density_arr(self):
+        config = sim.Configuration('pumha/data/config.dat')
+        env = sim.Landscape('pumha/data/islands2.dat')
+        puma = sim.PumaPopulation(env)
+        hare = sim.PumaPopulation(env)
+        pop_list = [hare, puma]
+        pop = puma.find_density_arr(sim.PumaPopulation, pop_list)
+        self.assertTrue(isinstance(pop, numpy.ndarray))
 
