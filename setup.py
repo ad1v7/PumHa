25323c531fe1b56d9325bf9697d5be032bc260d9
ad1v7 <marcin.kirsz@gmail.com>
2017-11-03 10:52:38 +0000

    updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='PumHa',
-    version='1.0.0',
+    version='1.0.1',
     description='Modelling pumas and hares in a landscape',
     long_description=long_description,
     url='https://github.com/ad1v7/PumHa',
